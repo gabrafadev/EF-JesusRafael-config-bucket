ed0c16c56c77cbcdac4391a029aa7dcc5eb3e66b
gabrafadev <desarrollo.sistemas.rafael@gmail.com>
2022-08-16 23:46:26 -0500

    mensaje

---
+++ src/test/java/com/idat/EFJesusRafaelConfigServer/EfJesusRafaelConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFJesusRafaelConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJesusRafaelConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
